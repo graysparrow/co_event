a9d32ee80018731b98238e91e6b32b69087078ed
graysparrow <363912650@qq.com>
2023-04-20 17:30:54 +0800

    提交异步事件逻辑代码

--- co_event.hpp
+++ co_event.hpp
@@ -2,6 +2,7 @@
 #define _CO_EVENT_H_
 
 #include <coroutine>
+#include <concepts>
 #include <exception>
 #include <memory>
 #include <iostream>
@@ -12,7 +13,16 @@ namespace co_event {
 
 enum class task_type: int {
 	Timer,
+	Event,
 	Max,
+};
+
+struct event_base {
+	virtual bool update_timer(int) {return false;}
+	virtual task_type type() const noexcept {return task_type::Event;}
+	template <typename EventType>
+		requires std::derived_from<EventType, event_base>
+	bool notice(std::shared_ptr<EventType>) const noexcept {return false;}
 };
 
 struct task {
@@ -21,9 +31,7 @@ struct task {
 
 	struct promise_type {
 		task get_return_object() {
-			return{
-				.h_ = handle_type::from_promise(*this)
-			};
+			return task(handle_type::from_promise(*this));
 		}
 		std::suspend_never initial_suspend() noexcept {
 			return {};
@@ -34,47 +42,61 @@ struct task {
 		void return_void() {}
 		void unhandled_exception() {}
 
-		task_type t_;
-		int param_;
+		template <typename EventType>
+			requires std::derived_from<EventType, event_base>
+		std::suspend_always await_transform(std::shared_ptr<EventType> event) {
+			event_ = event;
+			//printf("await_transform %d %d\n", event_->type(), event->type());
+			return {};
+		}
+
+		std::shared_ptr<event_base> event_;
 	};
 
-	bool operator () () {
+	task(handle_type h):h_(h), counter_(std::make_shared<int>(1)) {}
+	task(const task& rhs):h_(rhs.h_), counter_(rhs.counter_) {
+		++(*counter_);
+	}
+	task& operator=(const task& rhs) {
+		if (this != &rhs) {
+			h_ = rhs.h_;
+			counter_ = rhs.counter_;
+			++(*counter_);
+		}
+		return *this;
+	}
+	~task() {
+		if (--(*counter_) == 0) {
+			h_.destroy();
+			printf("h_ destroy\n");
+		}
+		h_ = 0;
+	}
+
+	void operator() () {
 		h_();
-		return h_.done();
 	}
 	bool done() {
 		return  h_.done();
 	}
 
-	task_type type() const noexcept {
-		return h_.promise().t_;
-	}
-	int param() const noexcept {
-		return h_.promise().param_;
-	}
-
-	void update_time_elapse(int elapse) {
-		if (h_.promise().t_ == task_type::Timer && h_.promise().param_ > 0) {
-			h_.promise().param_ -= elapse;
-			if (h_.promise().param_ <= 0) {
-				h_.resume();
-			}
-		}
-	}
-
 	handle_type h_;
+	std::shared_ptr<int> counter_;
 };
 
-struct timer {
-	timer(int time): time_(time) {}
-	bool await_ready() const noexcept {
+struct timer: public event_base {
+	virtual task_type type() const noexcept override {return task_type::Timer;}
+	bool update_timer(int elapse) noexcept {
+		//printf("update_timer\n");
+		if (time_ > 0) {
+			time_ -= elapse;
+			if (time_ <= 0) {
+				return true;
+			}
+		}
 		return false;
 	}
-	void await_suspend(task::handle_type h) {
-		h.promise().t_ = task_type::Timer;
-		h.promise().param_ = time_;
-	}
-	void await_resume() {}
+	timer(int time): time_(time) {}
 	int time_ = 0;
 };
 
@@ -88,17 +110,49 @@ private:
 	event_manager& operator=(const event_manager&) = delete;
 
 public:
-static std::shared_ptr<event_manager> create() {
-	return std::shared_ptr<event_manager>(new event_manager());
-}
+	static std::shared_ptr<event_manager> create() {
+		return std::shared_ptr<event_manager>(new event_manager());
+	}
 
 public:
 	void start_coroutine(task t) {
-		task_list.push_back(t);
+		task_list_.push_back(t);
+	}
+
+	void update_timer(int elapse) {
+		for (auto it = task_list_.begin(); it != task_list_.end();) {
+			auto& task = *it;
+			auto& event = task.h_.promise().event_;
+			if (event && event->type() == task_type::Timer && event->update_timer(elapse)) {
+				task();
+			}
+			if (task.done()) {
+				it = task_list_.erase(it);
+			} else {
+				++it;
+			}
+		}
+	}
+
+	template <typename EventType>
+		requires std::derived_from<EventType, event_base>
+	void notice_event(std::shared_ptr<EventType> event) {
+		for (auto it = task_list_.begin(); it != task_list_.end();) {
+			auto& task = *it;
+			auto event_ = std::dynamic_pointer_cast<EventType>(task.h_.promise().event_);
+			if (event_ && event_->type() == task_type::Event && event_->notice(event)) {
+				task();
+			}
+			if (task.done()) {
+				it = task_list_.erase(it);
+			} else {
+				++it;
+			}
+		}
 	}
 
 private:
-	std::list<task> task_list;
+	std::list<task> task_list_;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -5,11 +5,45 @@
 
 #include "co_event.hpp"
 
+struct recharge_event:public co_event::event_base {
+	recharge_event(int id):id_(id){}
+	bool notice(std::shared_ptr<recharge_event> event) {
+		printf("recharge_event notice\n");
+		if (event->id_ == id_) {
+			return true;
+		}
+		return false;
+	}
+	int id_;
+};
+
+auto event_mgr = co_event::event_manager::create();
+
+co_event::task timer_print() {
+	printf("begin timer_print\n");
+	for (int i = 0; i < 3; ++i) {
+		printf("timer print %d\n", i);
+		co_await std::make_shared<co_event::timer>(600);
+	}
+	co_await std::make_shared<recharge_event>(99);
+	printf("wait recharge ok\n");
+	printf("end timer_print\n");
+}
+
+co_event::task recharge_print() {
+	printf("begin recharge_print\n");
+	co_await std::make_shared<recharge_event>(99);
+	printf("end recharge_print\n");
+}
+
 void init() {
+	event_mgr->start_coroutine(timer_print());
+	//event_mgr->start_coroutine(recharge_print());
 }
 
 void update(int elapse) {
-	auto event_mgr = co_event::event_manager::create();
+	event_mgr->update_timer(elapse);
+	event_mgr->notice_event(std::make_shared<recharge_event>(99));
 }
 
 int main() {
